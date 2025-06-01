e2e551ca87c66c667789c3630fa757f49bbe5d7f
Du-22 <du88215@gmail.com>
2025-06-01 09:09:28 +0800

    添加首頁按鈕

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,44 @@
 import React, { useState } from "react";
-const Header = ({ currentNovel, onNovelSelect, novels = { novels } }) => {
+
+const Header = ({ currentNovel, onNovelSelect, novels, onHomeClick }) => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <header className="header">
-      <h1 className="novel-title">{currentNovel?.title || "請選擇小說"}</h1>
-      <div className="novel-selector">
-        <button
-          className="menu-button"
-          onClick={() => {
-            setShowMenu(!showMenu);
-          }}
-        >
-          ☰ 選擇小說
+      {/* 左側：首頁按鈕和標題 */}
+      <div className="header-left">
+        <button className="home-button" onClick={onHomeClick}>
+          🏠 首頁
         </button>
-        {showMenu && (
-          <div className="dropdown-menu">
-            {novels.map((novel) => (
-              <div
-                key={novel.id}
-                className="menu-item"
-                onClick={() => {
-                  onNovelSelect(novel);
-                  setShowMenu(false);
-                }}
-              >
-                {novel.title}
-              </div>
-            ))}
-          </div>
-        )}
+        <h1 className="novel-title">{currentNovel?.title || "小說閱讀器"}</h1>
+      </div>
+
+      {/* 右側：小說選擇選單 */}
+      <div className="header-right">
+        <div className="novel-selector">
+          <button
+            className="menu-button"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            ☰ 選擇小說
+          </button>
+          {showMenu && (
+            <div className="dropdown-menu">
+              {novels.map((novel) => (
+                <div
+                  key={novel.id}
+                  className="menu-item"
+                  onClick={() => {
+                    onNovelSelect(novel);
+                    setShowMenu(false);
+                  }}
+                >
+                  {novel.title}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
